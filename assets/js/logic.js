b3fa5318fb53cdd97af734fae43dec5c0d1198bb
Dylan Rutter <dylanrutter@Dylans-MacBook-Pro.local>
2018-05-19 10:52:48 -0500

    edits

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+var addressSearch
+var weatherSearch
+
+
+
+$(function () {
+  console.log("ready!");
+
+  // Capture Button Click
+  $("#search-btn").on("click", function (event) {
+    event.preventDefault();
+    console.log("click");
+
+  });
+});
